74080e81fdf151ff1babec5ffc576d3679411d4f
JJ-WSL <dashan1010zj@163.com>
2022-03-20 18:54:33 +0800

    init

---
+++ UNP.c
@@ -0,0 +1,23 @@
+#include "./src/lib/unp.h"
+
+void temp(void);
+
+int main(int argc, char *argv[])
+{
+    // 参数个数至少位 1
+    // printf("%d\n", argc);
+
+    // 第一个参数总是程序名
+    // /home/zhangjin/projects/unpv13e/UNP
+    // printf("%s\n", argv[0]);
+
+    printf("------ Start ------\n");
+    temp();
+    printf("------  End  ------\n");
+
+    return 0;
+}
+
+void temp(void)
+{
+}
