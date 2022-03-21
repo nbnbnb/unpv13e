12fd87d71ecaae4535fb4838dcdf392f98a0ea48
JJ-WSL <dashan1010zj@163.com>
2022-03-21 11:55:36 +0800

    cc

--- chap01/daytimetcpcli.c
+++ chap01/daytimetcpcli.c
@@ -26,7 +26,7 @@ int main(int argc, char **argv)
 	// 由于 TCP 套接字中 IP 和 端口号需要用特定的格式
 	// 所以调用
 	// 库函数 htons 去转换二进制端口号
-	// 库函数 inet_pton 去转换 IP 为合适的格式
+	// 库函数 inet_pton 去转换 IP 为合适的格式（支持 IPv6）
 
 	// 端口号转换
 	servaddr.sin_port = htons(53315);
@@ -37,18 +37,38 @@ int main(int argc, char **argv)
 		err_quit("inet_pton error for %s", ip);
 	}
 
+	// SA 是一个 "通用套接字结构"
+	// 每当一个套接字函数需要一个指向某个套接字地址结构的指针时
+	// 这个指针必须强制转换为一个通用套接字地址结构的指针
+	// 这是因为套接字函数早于 ANSI C 标准，20 世纪 80 年代早期开发这些函数时，ANSI C 的 void * 指针类型还不可用
+	// 问题是 "struct sockaddr" 长达 15 个字符，往往造成源代码行超出屏幕的右边缘，因此我们把它缩减成 SA
+
 	// connect 使用上面返回的描述符
+	// 第二个参数指定连接的服务器信息
 	if (connect(sockfd, (SA *)&servaddr, sizeof(servaddr)) < 0)
 	{
 		err_sys("connect error");
 	}
 
+	// 使用 read 函数读取服务器的应答
 	while ((n = read(sockfd, recvline, MAXLINE)) > 0)
 	{
+		// 使用 TCP 时必须小心，因为 TCP 是一个没有记录边界的字节流协议
+
+		// 如果服务器应该返回 26 个字节，那么这 26 个字节可以有多种返回方式
+		// 既可以是包含所有 26 个字节的单个 TCP 分节，也可以是每个分节只包含 1 个字节的 26 个 TCP 分节，或者其它任意组合
+
+		// 通常服务器返回包含所有 26 个字节的单个分节
+		// 但是如果数据量很大，我们就不能确保一次 read 调用能返回服务器的整个应答
+
+		// 因此从 TCP 套接字读取数据时，我们总是需要把 read 编写在某个循环中
+		// 当 read 返回 0（表明对端关闭连接）或负值（表明发生错误）时终止循环
+
 		/* null terminate */
 		// 结尾终止符
 		recvline[n] = 0;
 
+		// 使用标准 I/O 函数 fputs 输出结果
 		if (fputs(recvline, stdout) == EOF)
 		{
 			err_sys("fputs error");
--- chap01/daytimetcpsrv.c
+++ chap01/daytimetcpsrv.c
@@ -8,28 +8,58 @@ int main(int argc, char **argv)
 	char buff[MAXLINE];
 	time_t ticks;
 
+	//	1 Socket
 	listenfd = Socket(AF_INET, SOCK_STREAM, 0);
 
 	bzero(&servaddr, sizeof(servaddr));
 	servaddr.sin_family = AF_INET;
+
+	// 指定 IP 地址为 INADDR_ANY
+	// 这样要是服务器主机有多个网络接口，服务器进程就可以额在任意网路接口上接受客户端连接
 	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
 
 	// 服务端端口 53315
 	servaddr.sin_port = htons(53315);
 
+	// 2. Bind
 	Bind(listenfd, (SA *)&servaddr, sizeof(servaddr));
 
+	// 3. Listen
+	// 调用 listen 将套接字转换为一个监听套接字
+	// 这样来自客户端的外来连接就可以在改套接字上由内核接受
+	// LISTENQ 表示在这个监听描述符上排队的组大客户端连接数
 	Listen(listenfd, LISTENQ);
+
+	// 1 Socket，2. Bind，3. Listen
+	// TCP 服务器准备所谓监听描述符的正常步骤
 
 	for (;;)
 	{
+		// 通常情况下，服务器进程在 accept 调用中投入睡眠
+		// 等待某个客户连接的到达并被内核接受
+
+		// TCP 连接使用所谓的 "三路握手" 来建立连接
+		// 握手完毕时 accept 返回，其返回值是一个称为 "已连接描述符" 的新描述符
+
+		// 已连接描述符用于与新近连接的那个客户端通信
+
+		// accept 为每个连接到本服务器的客户端返回一个新描述符
 		connfd = Accept(listenfd, (SA *)NULL, NULL);
+
+		// time 返回 Unix 纪元
+		// 自 1970.1.1 毫秒数
 		ticks = time(NULL);
+
+		// ctime 把整数转换为直观可读的时间格式
 		snprintf(buff, sizeof(buff), "%.24s\r\n", ctime(&ticks));
+
+		// 将结果输出到客户端
 		Write(connfd, buff, strlen(buff));
 
 		printf("server response ...\n");
 
+		// 引发正常的 TCP 连接终止序列
+		// 每个方向上发送一个 FIN，每个 FIN 又由各自的对端确认
 		Close(connfd);
 	}
 
